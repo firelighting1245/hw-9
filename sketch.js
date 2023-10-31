5a4c412144d798dacb487e549c0570a00eee5e42
firelighting1245 <149473080+firelighting1245@users.noreply.github.com>
2023-10-31 17:30:08 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background("yellow");
+  //if right arrow is pressed
+  if (keyDown(RIGHT_ARROW)){
+    background("red")
+  }
+
+
+}
+
+
+
+
